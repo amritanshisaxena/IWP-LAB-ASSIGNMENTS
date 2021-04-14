8b203de8fd57872e194dd81c1ab23a9c19e0af3c
Amritanshi Saxena <49221982+amritanshisaxena@users.noreply.github.com>
2021-04-14 18:29:58 +0530

    Create Greatestof3.js

---
+++ Greatestof3.js
@@ -0,0 +1,5 @@
+        var a = prompt("Please enter 1st number to check");
+        var b = prompt("Please enter 2nd number to check");
+        var c = prompt("Please enter 3rd number to check");
+        alert("The greatest number is: "+ Math.max(a, b, c));
+
